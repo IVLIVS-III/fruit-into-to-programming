9432a5d283330280a31009ea6cb682d7e7e57613
Julius Bredemeyer <48645716+IVLIVS-III@users.noreply.github.com>
2023-10-15 19:08:24 +0200

    Add files via upload

---
+++ fruitl_template.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python3
+
+
+def choose_option(name):
+    ...
+
+
+def add_two_numbers():
+    ...
+
+
+def query_weather():
+    import requests
+    ...
+
+
+def search_beckonline():
+    import webbrowser
+    ...
+
+
+def main():
+    ...
+
+
+if __name__ == "__main__":
+    main()
